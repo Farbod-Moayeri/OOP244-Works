8a6c5c59d90f2420ffa28ebf12601a866064000a
fmoayeri2 <fmoayeri2@myseneca.ca>
2023-07-13 20:18:16 -0400

    MS3 Almost done

--- Project/MS2/LibApp.cpp
+++ Project/MS2/LibApp.cpp
@@ -1,6 +1,152 @@
-
+///////////////////////////////////////////////////////
+// MS2
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+// Section: NBB
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+#include <iostream>
 #include "LibApp.h"
+#include "Utils.h"
+
+using namespace std;
+
 namespace sdds {
-   
+	LibApp::LibApp()
+	{
+		m_changed = false;
+		m_mainMenu = "Seneca Library Application";
+		m_exitMenu = "Changes have been made to the data, what would you like to do?";
+
+		m_mainMenu << "Add New Publication" << "Remove Publication" << "Checkout publication from library" << "Return publication to library";
+		m_exitMenu << "Save changes and exit" << "Cancel and go back to the main menu";
+		load();
+	}
+	void LibApp::run()
+	{
+		bool keepRunning = true;
+		unsigned temp = 0;
+		unsigned temp2 = 0;
+		
+		while (keepRunning == true)
+		{
+			temp = m_mainMenu.run();
+
+			if (temp == 1)
+			{
+				newPublication();
+			}
+			else if (temp == 2)
+			{
+				removePublication();
+			}
+			else if (temp == 3)
+			{
+				checkOutPub();
+			}
+			else if (temp == 4)
+			{
+				returnPub();
+
+			}
+			else if (temp == 0)
+			{
+				if (m_changed == true)
+				{
+					m_exitMenu.run();
+
+					if (temp2 == 1)
+					{
+						save();
+						keepRunning = false;
+					}
+					else if (temp2 == 0)
+					{
+						if (confirm("Are you sure you want to quit without saving?") == true)
+						{
+							keepRunning = false;
+						}
+					}
+				}
+				else
+				{
+					keepRunning = false;
+				}
+			}
+
+
+		}
+
+		cout << "Goodbye...";
+	}
+	bool LibApp::confirm(const char* message)
+	{
+		bool isit = false;
+
+		if (message != nullptr)
+		{
+			Menu local(message);
+			local << "yes";
+			if (local.run() == 1)
+			{
+				isit = true;
+			}
+		}
+		
+		return isit;
+	}
+	void LibApp::load()
+	{
+		cout << "Loading Date" << endl;
+	}
+	void LibApp::save()
+	{
+		cout << "Saving Date" << endl;
+	}
+	void LibApp::search()
+	{
+		cout << "Seaching for publication" << endl;
+	}
+	void LibApp::returnPub()
+	{
+		search();
+		cout << "Returning publication" << endl;
+		cout << "Publication returned" << endl;
+		m_changed = true;
+	}
+	void LibApp::newPublication()
+	{
+		cout << "Adding new publication to library" << endl;
+		if (confirm("Add this publication to library?") == true)
+		{
+			m_changed = true;
+			cout << "Publication added" << endl;
+		}
+
+	}
+	void LibApp::removePublication()
+	{
+		cout << "Removing publication from library" << endl;
+		if (confirm("Remove this publication from library?") == true)
+		{
+			m_changed = true;
+			cout << "Publication removed" << endl;
+		}
+	}
+
+	void LibApp::checkOutPub()
+	{
+		search();
+		if (confirm("Check out publication?") == true)
+		{
+			m_changed = true;
+			cout << "Publication checked out" << endl;
+		}
+	}
+
 
 }
---
+++ Project/MS2/Menu.h
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////
+// MS2
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+// Section: NBB
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+#ifndef SDDS_MENU_H__
+#define SDDS_MENU_H__
+
+#include <iostream>
+
+namespace sdds {
+
+	const unsigned MAX_MENU_ITEMS = 20;
+
+	class Menu;
+
+	class MenuItem {
+		char* m_item{};
+		unsigned m_length{};
+		MenuItem(const char string[] = nullptr);
+		MenuItem(const MenuItem& src) = delete;
+		MenuItem& operator=(const MenuItem& src) = delete; 
+		~MenuItem();
+		operator bool()const;
+		operator const char* () const;
+		std::ostream& displayItem(std::ostream& ostr)const;
+		friend class Menu;
+	};
+
+	class Menu {
+		MenuItem* m_ItemArray[MAX_MENU_ITEMS]{};
+		unsigned m_numItems{};
+		MenuItem m_title{};
+		Menu(const Menu& src) = delete;
+		Menu& operator=(const Menu& src) = delete;
+		
+		
+		friend MenuItem;
+	public:
+
+		Menu(const char string[] = nullptr);
+		~Menu();
+		operator bool()const;
+		operator unsigned() const;
+		operator int() const;
+		unsigned operator~() const;
+		Menu& operator<<(const char* menuitemContent);
+		Menu& operator=(const char* title); //MS2: NEW
+		const char* operator[](unsigned index) const;
+		std::ostream& displayTitle(std::ostream& ostr = std::cout)const;
+		std::ostream& displayMenu(std::ostream& ostr = std::cout)const;
+		unsigned run() const;
+		
+	};
+
+	
+	std::ostream& operator<<(std::ostream& ostr, const Menu& right);
+
+}
+#endif // !SDDS_MENU_H__
