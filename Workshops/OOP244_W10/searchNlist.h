4e3dd084cb9ded046d73dd6104aa933bd8a4ca34
Farbod Moayeri <Farbod Moayeri@LAPTOP-54JRQ3A1>
2023-08-02 14:29:16 -0400

    W10: Added Student Citations

--- Workshops/OOP244_W10/searchNlist.h
+++ Workshops/OOP244_W10/searchNlist.h
@@ -1,3 +1,14 @@
+///////////////////////////////////////////////////////
+// Workshop 10
+// Name: Farbod Moayeri
+// Id: 134395227
+// Email: fmoayeri2@myseneca.ca
+///////////////////////////////////////////////////////
+// I have done all the coding by myself and only copied
+// the code that my professor provided to complete my 
+// workshops and assignments.
+///////////////////////////////////////////////////////
+
 #ifndef SDDS_SEARCHNLIST_H
 #define SDDS_SEARCHNLIST_H
 
